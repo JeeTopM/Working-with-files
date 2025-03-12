bd0e64fd87bd38a21ee09fc52367d99aaa345ad7
Лев Романович Панас <jieba2005@list.ru>
2025-03-12 18:20:54 +0300

    6-JSON-Elements ver.1

--- 3-4-Working-with-JSON-files/6-JSON-Elements.py
+++ 3-4-Working-with-JSON-files/6-JSON-Elements.py
@@ -10,13 +10,21 @@
 Программа должна вывести все пары ключ-значение введенного объекта, разделяя ключ и значение двоеточием, каждую на отдельной строке. 
 Если значением ключа является список, то все его элементы должны быть выведены через запятую.
 """
-
+import sys 
 import json
 
-json_file = input()
+text = sys.stdin.read()
+json_file = json.loads(text)
+
+for key, value in json_file.items():
+    if isinstance(value, list):
+        print(f'{key}: {", ".join(map(str, value))}')
+    else:
+        print(f"{key}: {value}")
 
 
-"""
+
+
 # 1
 {"size": 36, "style": "bold", "name": "text1", "alignment": "center"}
 
@@ -26,4 +34,15 @@ json_file = input()
  "name": "Cake", 
  "tastes": ["chocolate", "cream", "strawberry"]
 }
-"""
+
+# 3
+{
+"src": "Images/Sun.png",
+"name": "sun1",
+"hOffset": 250,
+"AAA": ["ABC", 123, 123, "XYZ"],
+"vOffset": 250,
+"alignment": "center",
+"key": [1, 2, 3, 4, 5],
+"another_key": ["a", "b", "c"]
+}
