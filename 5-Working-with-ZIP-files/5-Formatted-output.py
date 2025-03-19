ee0c6087fa94ac84d7e6d103dab00fe8cd69d4e4
Лев Романович Панас <jieba2005@list.ru>
2025-03-19 20:03:16 +0300

    5-Formatted-output v.1

--- 5-Working-with-ZIP-files/5-Formatted-output.py
+++ 5-Working-with-ZIP-files/5-Formatted-output.py
@@ -13,3 +13,18 @@
 """
 
 from zipfile import ZipFile
+from datetime import datetime
+
+file_path = "/Users/lev/Documents/Programming/Python/workbook.zip"
+with ZipFile(file_path) as zip_file:
+    data = [
+        (f.filename.split("/")[-1], datetime(*f.date_time), f.compress_size, f.file_size)
+        for f in zip_file.infolist()
+        if not f.is_dir()
+    ]
+    for numb, (name, dt, c_size, f_size) in enumerate(sorted(data)):
+        print(f'''{name}\n  Дата модификации файла: {dt}
+  Объем исходного файла: {f_size} байт(а)
+  Объем сжатого файла: {c_size} байт(а)''')
+        if len(data) > numb + 1:
+            print()
