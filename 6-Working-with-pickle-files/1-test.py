220a0b3fb44b8cf2bfc8ad0786372cfb8a4cd981
Лев Романович Панас <jieba2005@list.ru>
2025-03-21 13:24:23 +0300

    add 1-test v.1

---
+++ 6-Working-with-pickle-files/1-test.py
@@ -0,0 +1,22 @@
+"""
+Найдите и исправьте ошибки, допущенные в приведенной ниже программе, 
+чтобы она сериализовала словарь dogs и записала результат в файл dogs.pkl
+"""
+
+# Было
+"""
+import pickle
+
+dogs = {'Ozzy': 2, 'Filou': 7, 'Luna': 4, 'Skippy': 11, 'Barco': 13, 'Balou': 10, 'Laika': 15}
+
+with open('dogs.pkl', mode='rt') as file:
+    pickle.dumps(dogs, file)
+"""
+
+# Стало
+import pickle
+
+dogs = {'Ozzy': 2, 'Filou': 7, 'Luna': 4, 'Skippy': 11, 'Barco': 13, 'Balou': 10, 'Laika': 15}
+
+with open('dogs.pkl', mode='wb') as file:
+    pickle.dump(dogs, file)
